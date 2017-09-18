ccf83a3762c172dd9a6d7686cd5800e0a9854a21
Hao Yu Yeh <AngusYeh@my--Mac-1267.local>
2017-09-18 18:23:02 +1000

    adding two file

---
+++ EyesOfBlind/EyesOfBlind/TextToSpeech.swift
@@ -0,0 +1,22 @@
+//
+//  TextToSpeech.swift
+//  EyesOfBlind
+//
+//  Created by Hao Yu Yeh on 2017/9/18.
+//  Copyright © 2017年 Hao Yu Yeh. All rights reserved.
+//
+/*
+ This class "TextToSpeech" is used to transform a text into voice
+ */
+import Foundation
+import AVFoundation
+
+class TextToSpeech {
+    static let synth = AVSpeechSynthesizer()
+    
+    func say(txtIn:String) {
+        let utterance = AVSpeechUtterance(string: txtIn)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        TextToSpeech.synth.speak(utterance)
+    }
+}
