b54778ec1ce1c1e1b9d42004325c8915faa4a057
Hao Yu Yeh <AngusYeh@my--Mac-1267.local>
2017-09-18 18:17:19 +1000

    adding voice control function

--- EyesOfBlind/EyesOfBlind/HomeModeViewController.swift
+++ EyesOfBlind/EyesOfBlind/HomeModeViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import Speech
 
 class HomeModeViewController: UIViewController {
 
-    override func viewDidLoad() {
+    /*
+     variables for voice control
+     */
+    private var txtToSpeech = TextToSpeech()
+    private var voiceToText = SpeechToText()
+    //use to store the command which is converted from voice
+    private var voiceCommand = ""
+    
+    // MARK: UIViewController
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /*
+         define two gestures for responding different tasks
+         */
+        let signleTap = UITapGestureRecognizer(target: self, action: #selector(HomeModeViewController.handleSingleTap))
+        signleTap.numberOfTapsRequired = 1
+        self.view.addGestureRecognizer(signleTap)
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(HomeModeViewController.handleDoubleTap))
+        doubleTap.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(doubleTap)
+        // this will let singleTap not to perform when occurring double tap
+        signleTap.require(toFail: doubleTap)
+        // this will let the two functions beneath respond faster than touchesBegan()
+        signleTap.delaysTouchesBegan = true
+        doubleTap.delaysTouchesBegan = true
+        
+        // ask authentication for speech recognition
+        voiceToText.authentication()
+        // voice instruction for how to give voice command
+        while true {
+            if !TextToSpeech.synth.isSpeaking {
+                txtToSpeech.say(txtIn: "in home mode")
+                break
+            }
+        }
+        while true {
+            if !TextToSpeech.synth.isSpeaking {
+                txtToSpeech.say(txtIn: "touch the screen and then start to say command, touch again when finishing speaking")
+                break
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /*
+     touch screen one time to call run function in SpeechToText class:
+     first touch to activate the recording and then start to say command,
+     second touch when finishing speaking
+     */
+    func handleSingleTap() {
+        voiceToText.run()
     }
-
-
+    /*
+     touch screen two times to call returnTranscript function in SpeechToText class:
+     it will store the transcript of voice command in voiceCommand
+     */
+    func handleDoubleTap() {
+        voiceCommand = ""
+        voiceCommand = voiceToText.returnTranscript()
+        print("voice command: \(self.voiceCommand)")
+    }
 }
-
