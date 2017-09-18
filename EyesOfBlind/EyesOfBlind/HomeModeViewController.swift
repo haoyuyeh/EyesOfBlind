fcd9ea60a0d6f72a5d68184611f6326cd46422ec
Hao Yu Yeh <AngusYeh@my--Mac-1267.local>
2017-09-18 17:56:57 +1000

    create a new project set a default UI

---
+++ EyesOfBlind/EyesOfBlind/HomeModeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeModeViewController.swift
+//  EyesOfBlind
+//
+//  Created by Hao Yu Yeh on 2017/9/18.
+//  Copyright © 2017年 Hao Yu Yeh. All rights reserved.
+//
+
+import UIKit
+
+class HomeModeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
